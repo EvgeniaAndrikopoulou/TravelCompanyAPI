1ae8719f188cd93f51868eb66a9a53cf5b6d9e49
@evgeniaandrikopoulou <evgeniaandrikopoulou@gmail.com>
2024-11-10 18:53:26 +0200

    LocationService Tests commit

---
+++ AggregatedAPI_TravelCompanionTests/Utilities/MockHttpMessageHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AggregatedAPI_TravelCompanionTests.Utilities;
+
+public class MockHttpMessageHandler : HttpMessageHandler
+{
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        // This method will be overridden by the Moq setup.
+        return await Task.FromResult(new HttpResponseMessage());
+    }
+}
